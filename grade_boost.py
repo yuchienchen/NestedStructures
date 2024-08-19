37f373b632f3fdbd85d76c03a5d30adc52ed6a70
yuchienchen <yuchienc@yahoo.com>
2024-08-19 12:01:17 -0500

    first commit

---
+++ grade_boost.py
@@ -0,0 +1,28 @@
+
+def grade_boost(students, letter):
+    students = {
+    'Elyse': 5,
+    'Chris': 20,
+    'Mehran': 20,
+    'Clinton': 40,
+    'Neel': 12
+}
+
+    # letter = input("")
+    for key in students.keys():
+        if letter.upper() in key:
+            value = students[key] + 5
+            print(value)
+    
+    return students
+
+print(grade_boost({
+    'Elyse': 5,
+    'Chris': 20,
+    'Mehran': 20,
+    'Clinton': 40,
+    'Neel': 12
+}, 'C'))
+
+
+
