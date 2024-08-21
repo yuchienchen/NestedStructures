ab18cbede0a8c46c519570fd3a9d46b50af229a4
yuchienchen <yuchienc@yahoo.com>
2024-08-21 12:50:53 -0500

    Done!

--- Assignment6/rating_stats.py
+++ Assignment6/rating_stats.py
@@ -16,12 +16,40 @@ def calculate_rating_stats(filename):
 
     The resulting information is printed to the console.
     """
+    count_W = 0
+    count_W_high = 0
+    count_M = 0
+    count_M_high = 0
     with open(filename) as file:
         line = next(file)
         print(line)
-        # for line in file:
-        #     line = line.strip()
-        #     print(line)
+        for line in file:
+            line = line.strip()
+            # print(line)
+            parts = line.split(',')
+            rating = float(parts[0])
+            gender = parts[1]
+            if gender == 'W':
+                count_W += 1
+            else:
+                count_M += 1
+            
+            if gender == 'W' and rating > 3.5:
+                count_W_high += 1
+            if gender == 'M' and rating > 3.5:
+                count_M_high += 1
+
+        percentage_W_high = (count_W_high / count_W) * 100
+        percentage_M_high = (count_M_high / count_M) * 100
+        # print(count_W)
+        # print(count_M)
+
+        print(round(percentage_W_high))
+        print(round(percentage_M_high))
+
+        print(str(round(percentage_W_high)) + '% of reviews for women in the dataset are high.')
+        print(str(round(percentage_M_high)) + '% of reviews for men in the dataset are high.')
+
     # You fill this in.  Don't forget to remove the 'pass' statement above.
 
 
